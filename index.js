5eec171c181de3a76dce528928906bcbace9fc78
Nahuel Vattuone <nahuel.vattuone@gmail.com>
2023-12-03 12:19:16 -0300

    se adiciona Html, y JS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Funciones 
 function descuento(cantidad, valor) {
 
   if (cantidad >= 5) {
@@ -9,34 +9,7 @@ function descuento(cantidad, valor) {
   }
 }
 
-function producto(numero) {
-
-  switch (numero) {
-    case 1:
-      valorTotal = valorTotal + 12000;
-      break;
-    case 2:
-      valorTotal = valorTotal + 15000;
-      break;
-    case 3:
-      valorTotal = valorTotal + 10000;
-      break;
-    case 4:
-      valorTotal = valorTotal + 5000;
-      break;
-    case 5:
-      valorTotal = valorTotal + 2000;
-      break;
-    case 6:
-      valorTotal = valorTotal + 5000;
-      break;
-    default:
-      break;
-  }
-
-}
-
-
+// Inicio de programa
 
 let cantidadCompra = 0
 let valorTotal = 0
@@ -45,18 +18,41 @@ let caso = 1
 
 while (caso != 0) {
 
-  caso = prompt('Ingrese numero de producto a agregar al carrito\n 1-Monstera Deliciosa\n 2-Ficus Pandurata\n 3-Monstera Adansonii\n 4-Potus\n 5-Maceta N°16\n 6-Tierra 50L\n 0-Para Salir\n')
-  if (caso == undefined) {
-    caso = 0
-  } else if (caso >=0 && caso <= 6) {
-    producto(caso)
-    cantidadCompra++
+  caso = parseInt(prompt('Ingrese numero de producto a agregar al carrito\n 1-Monstera Deliciosa\n 2-Ficus Pandurata\n 3-Monstera Adansonii\n 4-Potus\n 5-Maceta N°16\n 6-Tierra 50L\n 0-Para Salir\n'))
+  
+  if (caso>=0 && caso<=6){ 
+  switch (caso) {
+    case 1:
+      valorTotal = valorTotal + 12000;
+      cantidadCompra ++ 
+      break;
+    case 2:
+      valorTotal = valorTotal + 15000;
+      cantidadCompra ++ 
+      break;
+    case 3:
+      valorTotal = valorTotal + 10000;
+      cantidadCompra ++ 
+      break;
+    case 4:
+      valorTotal = valorTotal + 5000;
+      cantidadCompra ++ 
+      break;
+    case 5:
+      valorTotal = valorTotal + 2000;
+      cantidadCompra ++ 
+      break;
+    case 6:
+      valorTotal = valorTotal + 5000;
+      cantidadCompra ++ 
+      break;
+    default:
+      break;
   }
-  else {
-    alert ("Ingrese numero de la lista")
   }
-
-
+  else{
+    alert("Ingrese un valor indicado")
+  }
 
 }
 
